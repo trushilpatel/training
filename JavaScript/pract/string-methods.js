dc86fbafce6c0b1b81af9d3c328b462cc7ffc5dc
trushilpatel9898 <trushilpatel9898@gmail.com>
2020-08-04 19:20:21 +0530

    JavaScript practicals

---
+++ JavaScript/pract/string-methods.js
@@ -0,0 +1,16 @@
+var string = "Kem chho majama";
+
+console.log(string.length)
+console.log(string.split(" "))
+console.log(string.charCodeAt(1))
+console.log(string[6])
+console.log(string)
+
+const country = {
+  name: 'india'
+}
+
+console.log(country)
+country.continent = "asia";
+console.log(country)
+
